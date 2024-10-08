4602f2d03d2e4b216bb9822e9917da88871f885f
Mikkelbilgrav1 <eaa23msb@students.eaaa.dk>
2024-10-08 11:25:28 +0200

    kurv start

--- burgerogfoot.js
+++ burgerogfoot.js
@@ -1,7 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
+/* document.addEventListener('DOMContentLoaded', () => {
     const burgerIcon = document.querySelector('.burger-icon');
     const menuOverlay = document.querySelector('.menu-overlay');
     const closeBtn = document.getElementById('close-icon');
+
 
     function toggleMenu() {
         menuOverlay.classList.toggle('active');
@@ -20,6 +21,53 @@ document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('click', (event) => {
         if (!menuOverlay.contains(event.target) && !burgerIcon.contains(event.target) && menuOverlay.classList.contains('active')) {
             closeMenu();
+        }
+    });
+}); */
+
+document.addEventListener('DOMContentLoaded', () => {
+    const burgerIcon = document.querySelector('.burger-icon');
+    const menuOverlay = document.querySelector('.menu-overlay');
+    const closeBtn = document.getElementById('close-icon');
+    const indkoebIcon = document.querySelector('.indkoeb-icon');
+    const indkoebOverlay = document.querySelector('.indkoeb-overlay');
+
+    function toggleMenu() {
+        menuOverlay.classList.toggle('active');
+        burgerIcon.classList.toggle('active');
+    }
+
+    function closeMenu() {
+        menuOverlay.classList.remove('active');
+        burgerIcon.classList.remove('active');
+    }
+
+    function toggleIndkoeb() {
+        indkoebOverlay.classList.toggle('active');
+        indkoebIcon.classList.toggle('active');
+    }
+
+    function closeIndkoeb() {
+        indkoebOverlay.classList.remove('active');
+        indkoebIcon.classList.remove('active');
+    }
+
+    burgerIcon.addEventListener('click', toggleMenu);
+    closeBtn.addEventListener('click', closeMenu);
+    indkoebIcon.addEventListener('click', toggleIndkoeb);
+    closeBtn.addEventListener('click', closeIndkoeb);
+
+
+    // Close menu when clicking outside
+    document.addEventListener('click', (event) => {
+        if (!menuOverlay.contains(event.target) && !burgerIcon.contains(event.target) && menuOverlay.classList.contains('active')) {
+            closeMenu();
+        }
+    });
+
+    document.addEventListener('click', (event) => {
+        if (!indkoebOverlay.contains(event.target) && !indkoebIcon.contains(event.target) && indkoebOverlay.classList.contains('active')) {
+            closeIndkoeb();
         }
     });
 });
